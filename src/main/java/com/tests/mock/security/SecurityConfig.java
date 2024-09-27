a6ace409f1a48c0fc80d18229284f38641b39115
SofiRoadToDev <sofi.workaffairs@gmail.com>
2024-09-27 18:20:18 -0300

    security added

---
+++ src/main/java/com/tests/mock/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.tests.mock.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .cors(cors ->cors.disable())
+                .authorizeHttpRequests(
+                        http -> http.requestMatchers("/api/v1/**").authenticated()
+                        ).build();
+    }
+}
